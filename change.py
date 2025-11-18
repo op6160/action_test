3767901e71f0c2120228d123360abea6e0a40823
op6161 <107234146+op6161@users.noreply.github.com>
2025-11-18 08:05:20 +0000

    Deploying to gh-pages from @ op6161/action_test@e132412c306430066154ece98944ffc24c5e4337 🚀

---
+++ change.py
@@ -0,0 +1,13 @@
+def log(msg):
+  print(f"action log: {msg}")
+#test
+
+with open("index.html", "w", encoding='utf-8') as file:
+  texts = file.readlines()
+  log(texts[0])
+  texts[0] = str(int(texts[0].split(",")[0])+1) + ","
+  log("changed.")
+  log(texts[0])
+  file.writelines(texts)
+
+log("fin")
